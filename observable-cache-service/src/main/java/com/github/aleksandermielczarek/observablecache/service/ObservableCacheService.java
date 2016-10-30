ee9dcb044d022d92bc68c6c4424b23497379954b
Aleksander Mielczarek <alekmiel91@gmail.com>
2016-10-30 19:36:49 +0100

    Rename modules

---
+++ observable-cache-service/src/main/java/com/github/aleksandermielczarek/observablecache/service/ObservableCacheService.java
@@ -0,0 +1,14 @@
+package com.github.aleksandermielczarek.observablecache.service;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Aleksander Mielczarek on 30.10.2016.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface ObservableCacheService {
+}
